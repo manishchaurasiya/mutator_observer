1955925f7c93b0b3ac63102557d30cad22f28cb8
Manish kumar <80481767+manishchaurasiya@users.noreply.github.com>
2022-11-28 15:32:53 +0530

    Update mutator_observer.js

--- mutator_observer.js
+++ mutator_observer.js
@@ -1,24 +1,25 @@
-function test(){
-    var config = { attributeFilter: [ "disabled", "value" ], attributeOldValue: true, subtree: false };
-   
-        var targetNode = document.querySelector(".delete_ip");
-    var observer = new MutationObserver(function(mutations) {
-        console.log(mutations)
-        mutations.forEach(function(mutation) {
-        
-        if (mutation.type == `attributes`) {
-            switch(mutation.attributeName) {
-                case `disabled`:
-                    mutation.target.setAttribute("disabled", true);
-                    break;
-                case `value`:
-                    mutation.target.setAttribute("value", mutation.oldValue);
-                    break;
-            }
-            observer.disconnect();
-            observer.observe(targetNode, config);
+(function () {
+        if ($('input[type=button]').hasClass('delete_ip')) {
+            $(document.querySelectorAll(".delete_ip")).each(function (index, element) {
+                var targetNode = element;
+                var config = { attributeFilter: ["disabled", "data-id"], attributeOldValue: true, subtree: false };
+                var observer = new MutationObserver(function (mutations) {
+                    mutations.forEach(function (mutation) {
+                        if (mutation.type == `attributes`) {
+                            switch (mutation.attributeName) {
+                                case `disabled`:
+                                    mutation.target.setAttribute("disabled", true);
+                                    break;
+                                case `data-id`:
+                                    mutation.target.setAttribute("data-id", mutation.oldValue);
+                                    break;
+                            }
+                            observer.disconnect();
+                            observer.observe(targetNode, config);
+                        }
+                    });
+                });
+                observer.observe(targetNode, config);
+            });
         }
-        });   
-    });
-    observer.observe(targetNode, config);
-}
+    })()
